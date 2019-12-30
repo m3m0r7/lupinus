4bc75c718c20a47fe4da47ead860c36a7abbaf3d
memory-agape <memory.style.serv@gmail.com>
2019-12-30 19:26:47 +0900

    WIP

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
-	fmt.Printf("Hello World!")
+	go func() {
+		listener, _ := net.Listen(
+			"tcp",
+			"0.0.0.0:30000",
+		)
+		fmt.Printf("Start client accepting server %v\n", listener.Addr())
+		for {
+			connection, _ := listener.Accept()
+			go func() {
+				fmt.Printf("[Client] Connected from %v\n", connection.RemoteAddr())
+				connection.Close()
+			}()
+		}
+	}()
+
+	listener, _ := net.Listen(
+		"tcp",
+		"0.0.0.0:31000",
+	)
+
+	fmt.Printf("Start camera receiving server %v\n", listener.Addr())
+	for {
+		connection, _ := listener.Accept()
+		go func() {
+			fmt.Printf("[CAMERA] Connected from %v\n", connection.RemoteAddr())
+			connection.Close()
+		}()
+	}
 }
