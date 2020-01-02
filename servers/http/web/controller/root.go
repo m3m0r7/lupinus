9ffdfaa153961ed7577c63f6da9b2da0eb8e0689
memory-agape <memory.style.serv@gmail.com>
2020-01-02 19:01:54 +0900

    Update

--- servers/http/web/controller/root.go
+++ servers/http/web/controller/root.go
@@ -5,8 +5,7 @@ import "../../../http"
 func RequestRoot(clientMeta http.HttpClientMeta)  (*http.HttpBody, *http.HttpHeader) {
 	body := http.HttpBody{
 		Payload: map[string]interface{}{
-			"code": -1,
-			"message": ":gopher:",
+			"message": "(=^・_・^=)",
 		},
 	}
 
