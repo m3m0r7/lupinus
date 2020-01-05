4e2ac1d9dc3d225222b84d9259b756336ca7640e
memory-agape <memory.style.serv@gmail.com>
2020-01-05 17:35:11 +0900

    Update

--- start.sh
+++ start.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+while :
+  do
+    echo "Waiting for upon docker."
+    docker ps 1>/dev/null 2>&1
+
+    if [ $? == 0 ]; then
+      echo "Docker is running"
+      break
+    fi
+    echo "."
+    sleep 1
+  done
+
 docker-compose up -d
 source /etc/systemd/env
 /bin/go run main.go
