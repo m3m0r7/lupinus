2b99b2dae56d79b41815147aeb681a7a6f4e8453
memory-agape <memory.style.serv@gmail.com>
2020-01-13 23:27:17 +0900

    Update

--- util/utility.go
+++ util/utility.go
@@ -1,30 +1,33 @@
 package util
 
 import (
-	"math"
 	"strings"
 )
 
 func Chunk(data []byte, size int) ([][]byte, int) {
-	loops := int(math.Floor(float64(len(data)) / float64(size)))
-	chunks := make([][]byte, loops)
-	for i := 0; i < loops; i++ {
-		chunks[i] = data[(size * i):(size * (i + 1))]
-	}
-
-	lastPos := loops * size
-	// Read remaining data
-	remainBytes := len(data) - lastPos
-	if remainBytes > 0 {
-		// Expand slices
-		newChunks := make([][]byte, loops + 1)
-		newChunks = append(
-			newChunks,
-			chunks...,
-		)
-		newChunks[loops] = data[lastPos:(lastPos + remainBytes)]
-	}
-	return chunks, loops
+	chunks := make([][]byte, 1)
+	chunks[0] = data
+	return chunks, 1
+	// TODO: FIX bottom code
+	//	loops := int(math.Floor(float64(len(data)) / float64(size)))
+	//	chunks := make([][]byte, loops)
+	//	for i := 0; i < loops; i++ {
+	//		chunks[i] = data[(size * i):(size * (i + 1))]
+	//	}
+	//
+	//	lastPos := loops * size
+	//	// Read remaining data
+	//	remainBytes := len(data) - lastPos
+	//	if remainBytes > 0 {
+	//		// Expand slices
+	//		newChunks := make([][]byte, loops + 1)
+	//		newChunks = append(
+	//			newChunks,
+	//			chunks...,
+	//		)
+	//		newChunks[loops] = data[lastPos:(lastPos + remainBytes)]
+	//	}
+	//	return chunks, loops
 }
 
 func SplitWithFiltered(data string, sep string) []string {
